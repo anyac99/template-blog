193ed37a32d7a97c0e98acea1b19d1474bd21895
Anya Collins <anyacollins@anyas-air.lan>
2021-10-21 11:54:39 -0700

    user authentication

--- main/__init__.py
+++ main/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '781526a5ddc485f1e3c0f0ff0cde5559'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from main import routes
