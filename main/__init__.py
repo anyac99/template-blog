da81ea182affb559d85a0bd1f9e3ba69e1dace22
Anya Collins <anyacollins@anyas-air.lan>
2021-10-28 15:27:30 -0700

    blueprints/configuration + added gitignore

--- main/__init__.py
+++ main/__init__.py
@@ -1,29 +1,35 @@
-import os
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from main.config import Config
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '781526a5ddc485f1e3c0f0ff0cde5559'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-mail_settings = {
-    "MAIL_SERVER": 'smtp.gmail.com',
-    "MAIL_PORT": 587,
-    "MAIL_USE_TLS": True,
-    "MAIL_USE_SSL": False,
-    "MAIL_USERNAME": os.environ['EMAIL_USER'],
-    "MAIL_PASSWORD": os.environ['EMAIL_PASS']
-}
 
-app.config.update(mail_settings)
-mail = Mail(app)
+mail = Mail()
 
-from main import routes
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from main.users.routes import users
+    from main.posts.routes import posts
+    from main.primary.routes import primary
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(primary)
+
+    return app
+
--- main/models.py
+++ main/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timezone
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from main import db, login_manager, app
+from flask import current_app
+from main import db, login_manager
 from flask_login import UserMixin
 
 
@@ -22,12 +23,12 @@ class User(db.Model, UserMixin):
     posts = db.relationship('Post', backref='author', lazy=True)
 
     def get_reset_token(self, expires_sec=1800):
-        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
         return s.dumps({'user_id': self.id}).decode('utf-8')
 
     @staticmethod
     def verify_reset_token(token):
-        s = Serializer(app.config['SECRET_KEY'])
+        s = Serializer(current_app.config['SECRET_KEY'])
         try:
             user_id = s.loads(token)['user_id']
         except:
