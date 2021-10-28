1673bf3915481c1e8a8b0e9d25944b06cc4d07eb
Anya Collins <anyacollins@anyas-air.lan>
2021-10-28 10:54:26 -0700

    can send emails, users can reset password

--- main/__init__.py
+++ main/__init__.py
@@ -1,7 +1,10 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '781526a5ddc485f1e3c0f0ff0cde5559'
@@ -11,5 +14,16 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+mail_settings = {
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 587,
+    "MAIL_USE_TLS": True,
+    "MAIL_USE_SSL": False,
+    "MAIL_USERNAME": os.environ['EMAIL_USER'],
+    "MAIL_PASSWORD": os.environ['EMAIL_PASS']
+}
+
+app.config.update(mail_settings)
+mail = Mail(app)
 
 from main import routes
